0a3bcafc73229d80734c971d16df8743d06ca74c
jyoti <jshindems@gmail.com>
2020-06-26 12:47:18 -0400

    variables and data types

---
+++ script.js
@@ -0,0 +1,31 @@
+console.log("Hello World")
+
+//ES5
+//variables and data types
+//declare a variable
+var firstName= 'Jo';
+console.log(firstName);
+
+//string
+var lastName = 'Shinde';
+
+//number
+var age = 24;
+
+//boolean
+var myName = true;
+console.log(myName)
+
+//undefined
+var myAge;
+console.log(myAge);
+
+//variable naming rules
+var _3years = 3;
+var jobTitle = "Software Developer"
+var $experience = 3;
+
+console.log(_3years);
+console.log(jobTitle);
+console.log($experience)
+
