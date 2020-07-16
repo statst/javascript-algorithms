08296feb672c805e2e4e3637d52c58c222988c6c
jyoti <jshindems@gmail.com>
2020-07-16 11:00:36 -0400

    primitives vs objects

--- script.js
+++ script.js
@@ -437,3 +437,40 @@ var jane = Object.create(personProto, {
     yearOfBirth: { value: 1969},
     job: {value: 'designer'}
 })
+
+//primitives vs objects
+//when we pass a primitive into an object a simple copy is created, it will never affect a variable outside
+//when we change object inside of a function, it is still reflected outside of a function
+//primitives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+var obj1 = {
+    name: 'John',
+    age: 26
+}
+
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
+
+//functions
+var age =27;
+var obj = {
+    name: 'Jonas',
+    city: 'Libson'
+}
+
+function change(a, b){
+    a = 30;
+    b.city = 'San Francisco'
+}
+
+change(age, obj);
+
+console.log(age);
+console.log(obj.city);
