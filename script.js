8b98445281d7dde30260907e0eaf987b417f8344
jyoti <jshindems@gmail.com>
2020-07-16 10:50:03 -0400

    objects

--- script.js
+++ script.js
@@ -416,3 +416,24 @@ mark.calculateAge();
 console.log(john.lastName);
 console.log(jane.lastName);
 console.log(mark.lastName);
+
+
+//object.create
+var personProto = {
+    calculateAge: function(){
+        console.log(2016 - this.yearOfBirth)
+    }
+};
+
+var john = Object.create(personProto);
+
+john.name = 'John'
+john.yearOfBirth = 1990;
+john.job= 'teacher'
+
+
+var jane = Object.create(personProto, {
+    name: { value: 'Jane'},
+    yearOfBirth: { value: 1969},
+    job: {value: 'designer'}
+})
