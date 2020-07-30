85f3e9318e166b5ab307782231b1011b5ee8744a
jyoti <jshindems@gmail.com>
2020-07-30 10:15:39 -0400

    added isprime number solution

--- script.js
+++ script.js
@@ -722,10 +722,32 @@ function balancedParens(string) {
 
 
 //Template String
-function getMessage(){
-    const year = new Date().getFullYear();
-    return `The year is ${year}`;
+// function getMessage(){
+//     const year = new Date().getFullYear();
+//     return `The year is ${year}`;
+// }
+
+
+// console.log(getMessage());
+
+
+// Define a function that takes an integer argument and returns logical value true or false depending on if the integer is a prime.
+
+// Per Wikipedia, a prime number (or a prime) is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+
+// Requirements
+// You can assume you will be given an integer input.
+// You can not assume that the integer will be only positive. You may be given negative numbers as well (or 0).
+// NOTE on performance: There are no fancy optimizations required, but still the most trivial solutions might time out. Numbers go up to 2^31 (or similar, depends on language version). Looping all the way up to n, or n/2, will be too slow.
+
+function isPrime(num) {
+	if (num < 2) {
+		return false;
+	}
+	for (let i = 2; i < num / 2 + 1; i++) {
+		if (num % i === 0) {
+			return false;
+		}
+	}
+	return true;
 }
-
-
-console.log(getMessage());
