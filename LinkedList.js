55398a3afcbe025aa20bd22bede69d42f0c2874f
jyoti <jshindems@gmail.com>
2020-10-12 13:41:14 -0400

    added shift method

--- LinkedList.js
+++ LinkedList.js
@@ -1,19 +1,18 @@
-class Node {
-	constructor(val) {
+class Node{
+	constructor(val){
 		this.val = val;
-		this.next = null;
+		this.next = null
 	}
 }
-
 class SinglyLinkedList {
-	constructor() {
+	constructor(){
 		this.head = null;
 		this.tail = null;
 		this.length = 0;
 	}
-	push(val) {
-		var newNode = new Node(val);
-		if (!this.head) {
+	push(val){
+		let newNode = new Node(val);
+		if(!this.head){
 			this.head = newNode;
 			this.tail = this.head;
 		} else {
@@ -23,8 +22,52 @@ class SinglyLinkedList {
 		this.length++;
 		return this;
 	}
+	pop(){
+		//if there are no nodes in the list, return undefined
+		if (!this.head) return undefined;
+		let current = this.head;
+		let newTail = current;
+		//loop through the list until reach tail
+		while (current.next) {
+			newTail = current;
+			current = current.next;
+		}
+		this.tail = newTail;
+		//set the tail to be the second to last node
+		this.tail.next = null;
+		//decrement the length of the list by 1
+		this.length--
+		if(this.length === 0){
+			this.head = null;
+			this.tail = null;
+		}
+		//return the value of the node removed
+		return current;
+	}
+	shift() {
+		//if there are no nodes in the list, return undefined
+		if (!this.head) return undefined;
+		//store current head property in the variable
+		let currentHead = this.head;
+		//set the head property to be the current head's next property
+		this.head = currentHead.next;
+		//decrement length by 1
+		this.length--;
+		if (this.length === 0) {
+			this.tail = null;
+		}
+		//return the value of the node removed
+		return currentHead;
+	}
 }
+
+
+
 
 let linkedList = new SinglyLinkedList();
 console.log(linkedList.push("Hi"))
-console.log(linkedList.push("Hello"))
+console.log(linkedList.push("How"))
+console.log(linkedList.push("are"));
+console.log(linkedList.push("you"));
+console.log(linkedList.push("Jo"));
+console.log(linkedList.shift());
