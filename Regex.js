40f67a882a99bdbfdcd506a25ab8a050d07aae7f
jyoti <jshindems@gmail.com>
2020-12-01 23:14:38 -0500

    added sum solution

--- Regex.js
+++ Regex.js
@@ -10,3 +10,56 @@ let result = twinkleStar.match(starRegex);
 
 //Match Anything with Wildcard Period
 
+// The wildcard character . will match any one character. The wildcard is also called dot and period. To match "hug", "huh", "hut", and "hum", we can use the regex /hu./ to match all four words.
+
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /.un/; 
+let result = unRegex.test(exampleStr);
+
+// Character classes allow you to define a group of characters you wish to match by placing them inside square ([ and ]) brackets.
+
+// For example, you want to match "bag", "big", and "bug" but not "bog". You can create the regex /b[aiu]g/ to do this. The [aiu] is the character class that will only match the characters "a", "i", or "u".
+
+let quoteSample =
+	'Beware of bugs in the above code; I have only proved it correct, not tried it.';
+let vowelRegex = /[aeiou]/gi; 
+let result = quoteSample.match(vowelRegex);
+
+// Inside a character set, you can define a range of characters to match using a hyphen character: -.
+
+// For example, to match lowercase letters a through e you would use [a-e].
+
+let quoteSample = "The quick brown fox jumps over the lazy dog.";
+let alphabetRegex = /[a-z]/gi; 
+let result = quoteSample.match(alphabetRegex); 
+
+// the Alphabet
+// Using the hyphen (-) to match a range of characters is not limited to letters. It also works to match a range of numbers.
+
+// For example, /[0-5]/ matches any number between 0 and 5, including the 0 and 5.
+
+// Also, it is possible to combine a range of letters and numbers in a single character set.
+
+let quoteSample = "Blueberry 3.141592653s are delicious.";
+let myRegex = /[h-s2-6]/ig; 
+let result = quoteSample.match(myRegex); 
+
+// create a set of characters that you do not want to match. These types of character sets are called negated character sets.
+
+// To create a negated character set, you place a caret character (^) after the opening bracket and before the characters you do not want to match.
+
+// For example, /[^aeiou]/gi matches all characters that are not a vowel. Note that characters like ., !, [, @, / and white space are matched - the negated vowel character set only excludes the vowel characters.
+
+let quoteSample = "3 blind mice.";
+let myRegex = /[^aeiou0-9]/ig; 
+let result = quoteSample.match(myRegex); 
+
+// Sometimes, you need to match a character (or group of characters) that appears one or more times in a row. This means it occurs at least once, and may be repeated.
+
+// You can use the + character to check if that is the case. Remember, the character or pattern has to be present consecutively. That is, the character has to repeat one after the other.
+
+// For example, /a+/g would find one match in "abc" and return ["a"]. Because of the +, it would also find a single match in "aabc" and return ["aa"].
+
+let difficultSpelling = 'Mississippi';
+let myRegex = /s+/g; 
+let result = difficultSpelling.match(myRegex);
